c587b72a39ba6ca3acec768aa5ad0626a60a23b7
Richard Henry <richardhenry@me.com>
2022-05-06 04:00:49 -0500

    emoji: update to unicode 14.0

--- fetch_emoji.py
+++ fetch_emoji.py
@@ -36,8 +36,8 @@ FILTER_KEYWORD_CHARACTERS = {'(', ')', '-', '“', '”', '.', ','}
 
 
 @click.command()
-@click.option('--url', default='https://www.unicode.org/emoji/charts-13.1/emoji-list.html')
-@click.option('--skintone-url', default='https://www.unicode.org/emoji/charts-13.1/full-emoji-modifiers.html')
+@click.option('--url', default='https://www.unicode.org/emoji/charts-14.0/emoji-list.html')
+@click.option('--skintone-url', default='https://www.unicode.org/emoji/charts-14.0/full-emoji-modifiers.html')
 @click.option('--extra-keywords', help='CSV of extra keywords', required=True)
 @click.option('--fe0f', help='CSV of characters that require an emoji variation selector to render correctly', required=True)
 @click.option('--exclude', help='CSV of characters to exclude', required=True)
